b332e3f38a29a39acf344656a99e2d76838b0fe4
Rokas <rokas@estotyvilnius.com>
2026-04-13 16:12:49 +0300

    add two other tasks

--- Queue&Stack/Program.cs
+++ Queue&Stack/Program.cs
@@ -30,6 +30,69 @@
 
       ReverseFirstK(queueNumbers, k);
       Console.WriteLine(string.Join(", ", queueNumbers));
+
+      //--2--//
+      Console.Write("Enter a string: ");
+      string input = Console.ReadLine().ToLower().Replace(" ", "");
+
+      Queue<char> queue = new Queue<char>();
+      Stack<char> stack = new Stack<char>();
+
+      foreach (char c in input)
+      {
+        queue.Enqueue(c);
+        stack.Push(c);
+      }
+
+      bool isPalindrome = true;
+      while (queue.Count > 0)
+      {
+        if (queue.Dequeue() != stack.Pop())
+        {
+          isPalindrome = false;
+          break;
+        }
+      }
+
+      Console.WriteLine(isPalindrome ? "Is a palindrome!" : "Not a palindrome.");
+
+      //--3--//
+      Console.Write("Enter a string: ");
+      string line = Console.ReadLine();
+
+      Stack<char> brackets = new Stack<char>();
+      bool isValid = true;
+
+      foreach (char c in line)
+      {
+        if (c == '(' || c == '[' || c == '{')
+        {
+          brackets.Push(c);
+        }
+        else if (c == ')' || c == ']' || c == '}')
+        {
+          if (brackets.Count == 0)
+          {
+            isValid = false;
+            break;
+          }
+
+          char top = brackets.Pop();
+
+          if (c == ')' && top != '(' ||
+              c == ']' && top != '[' ||
+              c == '}' && top != '{')
+          {
+            isValid = false;
+            break;
+          }
+        }
+      }
+
+      if (brackets.Count > 0)
+        isValid = false;
+
+      Console.WriteLine(isValid ? "Brackets are valid!" : "Brackets are invalid.");
     }
 
     static int RandomNumber(Random rnd, int min, int max)
